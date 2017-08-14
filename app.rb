397c671ad9eba542d32001093674ceddacdf47c5
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-14 16:19:03 +0100

    separating views

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 
 
-get '/cat' do
-'<img src="http://www.wapppictures.com/wp-content/uploads/2015/12/290983-sheldon-cooper-in-the-big-bang-theory-min.jpg" style="border: 2px dashed red">'
+get '/meme' do
+  erb(:index)
 end
-
