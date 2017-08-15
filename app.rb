ca74e2490410adeeb3b6d2469671a389dadc3f80
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-15 16:09:40 +0100

    Sintra post-ed params complete

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 
-get '/random-meme' do
-  @random_name = "#{['Raj', 'Howard', 'Penny', 'Sheldon', 'Leonard'].sample}"
-  erb(:index)
+# get '/random-meme' do
+#   @random_name = "#{['Raj', 'Howard', 'Penny', 'Sheldon', 'Leonard'].sample}"
+#   erb(:index)
+# end
+
+get '/form-meme' do
+  erb(:form_meme)
 end
 
-get '/named-meme' do
+post '/named-meme' do
   p params
   @random_name = params[:name]
   erb(:index)
